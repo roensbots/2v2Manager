7c41507ce2da15a88bfa9a6c87afb792d121273e
roenw <roenw@icloud.com>
2018-06-07 20:51:52 -1000

    It's a start, I guess...

--- bot.py
+++ bot.py
@@ -2,4 +2,23 @@
 
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
+from discord.ext.commands import bot
+import time
+
+client = discord.Client()
+ 
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+ 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        userID = message.author.id
+        await client.send_message(message.channel, '<@%s> pong' % (userID))
+
+
+client.run("")
